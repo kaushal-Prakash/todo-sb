86eb61ebd006776a93daa88f1bbb3a5af4f591a7
devShadow <147122977+kaushal-Prakash@users.noreply.github.com>
2025-11-02 20:34:02 +0530

    Added basic user signup

--- src/main/java/com/todo/TodoApp/Controllers/AuthController.java
+++ src/main/java/com/todo/TodoApp/Controllers/AuthController.java
@@ -1,5 +1,38 @@
 package com.todo.TodoApp.Controllers;
 
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.auth0.jwt.algorithms.Algorithm;
+import com.todo.TodoApp.Models.User;
+import com.todo.TodoApp.Services.AuthService;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/auth")
 public class AuthController {
 	
+	@Autowired
+	private AuthService service;
+	
+	private static final String secret = "MySecretStringIs****";
+	private static final Algorithm algo = Algorithm.HMAC256(secret);
+	
+	@PostMapping("/signup")
+	public ResponseEntity<?> signupMap(@RequestBody User u) {
+		return ResponseEntity.ok().body(Map.of("message",service.userSignup(u)));
+	}
+	
+	@GetMapping("/")
+	public ResponseEntity<?> getAll(){
+		return ResponseEntity.ok(Map.of("data",service.getAll()));
+	}
 }
--- src/main/java/com/todo/TodoApp/Services/AuthService.java
+++ src/main/java/com/todo/TodoApp/Services/AuthService.java
@@ -1,8 +1,27 @@
 package com.todo.TodoApp.Services;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.todo.TodoApp.Models.User;
+import com.todo.TodoApp.Repos.AuthRepo;
 
 @Service
 public class AuthService {
+	@Autowired
+	private AuthRepo repo;
 	
+	public String userSignup(User u) {
+		if(!(repo.findById(u.getUsername()).isEmpty())) {
+			return "User Already Registered";
+		}
+		repo.save(u);
+		return "User created Successfully";
+	}
+	
+	public List<User> getAll(){
+		return repo.findAll();
+	}
 }
